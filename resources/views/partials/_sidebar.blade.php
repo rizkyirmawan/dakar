4a773b19949262ff925c0f0019ddbd6eb247b138
Rizky Irmawan <rzkyirmwn@gmail.com>
2021-04-10 11:04:53 +0700

    Removed alpha from sidebar

--- resources/views/partials/_sidebar.blade.php
+++ resources/views/partials/_sidebar.blade.php
@@ -6,7 +6,7 @@
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">DAKAR <sup>ALPHA</sup></div>
+        <div class="sidebar-brand-text mx-3">DAKAR</div>
     </a>
 
     <!-- Divider -->
